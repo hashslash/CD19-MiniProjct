6ee2c5aef416dbc8a46f79955c75897b406cdda5
Sushanth Kille <30801615+sushanthkille@users.noreply.github.com>
2019-02-22 20:41:17 +0530

    Till Parser Table 0.1

--- Library/Grammar.py
+++ Library/Grammar.py
@@ -26,14 +26,44 @@ class Grammar:
             self.__terminals = list(set(self.__terminals))
         return self.__variables
 
+    def get_terminal_list(self):
+        if len(self.__terminals) == 0:
+            # list all variables
+            for i in self.productions:
+                self.__variables.append(i.variable)
+            self.__variables = list(set(self.__variables))
+            # list all terminals
+            for i in self.productions:
+                for j in i.rhs:
+                    if type(j) == TokenIdentifier:
+                        self.__terminals.append(j)
+            self.__terminals = list(set(self.__terminals))
+        return self.__terminals
+
     def add_production(self, production):
         self.productions.append(production)
 
     def first(self, variable):
         prods = []
+        first = {}
         for i in self.productions:
             if i.variable == variable:
-                prods.append(variable)
+                prods.append(i)
+
+        for i in prods:
+            if type(i.rhs[0]) is TokenIdentifier:
+                try:
+                    first[i.rhs[0]].append(i)
+                except Exception:
+                    first[i.rhs[0]] = [i]
+            else:
+                ter = self.first(i.rhs[0])
+                for k in ter:
+                    try:
+                        first[k].append(i)
+                    except Exception:
+                        first[k] = [i]
+        return first
 
     def follow(self, variable):
         return
--- Main.py
+++ Main.py
@@ -29,12 +29,30 @@ code = str(code_file.read())
 parser = Parser(tid_s, grammar, code)
 
 print("-----------***parse table***--------------")
+
 parse_table = parser.get_parse_table()
+terminals = grammar.get_terminal_list()
+print("variables".center(10), end="|")
+for i in terminals:
+    print("|", str(i).center(49), end="|")
+print()
 for i in parse_table:
-    for j in parse_table[i]:
-        print(i, "| (", j, ") |", parse_table[i][j])
-print("--------------------------------------------\n\n")
+    print(str(i).center(10), end="|")
+    for j in terminals:
+        print("|", end="")
+        try:
+            pr = ""
+            for k in parse_table[i][j]:
+                pr += str(k)+","
+            print(pr[:-1].center(50), end="|")
+        except Exception:
+            print("".center(50), end="|")
+    print()
+
+print("\n--------------------------------------------\n\n")
 
 print("------------***Parse Tree***---------------")
+
 print(str(parser.get_parse_tree()))
+
 print("--------------------------------------------\n\n")
