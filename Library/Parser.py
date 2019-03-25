8d0773b874d4e71e7583803b02b9253cc19c7b0c
Sushanth Kille <30801615+sushanthkille@users.noreply.github.com>
2019-03-25 20:09:56 +0530

    sync implemented in parse table

--- Library/Parser.py
+++ Library/Parser.py
@@ -16,8 +16,8 @@ class Parser:
 
     def __generate_parse_table(self):
         variables = self.grammar.get_variable_list()
-
         for i in variables:
+            sync = True
             self.parse_table[i] = {}
             fir = self.grammar.first(i)
             for j in fir:
@@ -27,12 +27,17 @@ class Parser:
                     except:
                         self.parse_table[i][j] = fir[j]
                 else:
+                    sync = False
                     fol = self.grammar.follow(i)
                     for k in fol:
                         try:
                             self.parse_table[i][k] += fir[j]
                         except:
                             self.parse_table[i][k] = fir[j]
+            if sync:
+                for j in self.grammar.follow(i):
+                    self.parse_table[i][j] = ["sync"]
+
         for i in self.parse_table:
             for j in self.parse_table[i]:
                 if len(self.parse_table[i][j]) > 1:
@@ -81,6 +86,7 @@ class Parser:
 
     def __parse(self, token_id, data):
         '''
+
         lexer = LexicalAnalyser(token_id, data)
         self.parse_tree = self.__tree(Variable("St"), lexer)
 
@@ -103,6 +109,8 @@ class Parser:
                     return
                 else:
                     print("\nError Occured at >>", next_token.lexeme)
+                    print("Parsing Failed")
+                    exit(0)
             else:
                 # print("stack-----", str(stack))
                 top_of_stack = stack.pop()
@@ -118,13 +126,14 @@ class Parser:
                 print(str(next_token).center(50, " "), end="|")
                 # print("token-", next_token.lexeme)
                 # check if input is at end
-                if next_token is None or next_token.id == "eoc" and type(top_of_stack) is TokenIdentifier and top_of_stack.id != "eoc":
+                if next_token is None or next_token.id == "eoc" and type(
+                        top_of_stack) is TokenIdentifier and top_of_stack.id != "eoc":
                     print("Error")
                     return
                 if type(top_of_stack) is TokenIdentifier:
                     if top_of_stack.id == next_token.id:
                         next_token = None
-                        print(str("match " + str(top_of_stack)).center(50),end="|")
+                        print(str("match " + str(top_of_stack)).center(50), end="|")
                     else:
                         print("\nExpected", top_of_stack.id, "Found", next_token.id)
                         print()
@@ -155,7 +164,7 @@ class Parser:
         print(str("Input").center(50, " "), end="|")
         print(str("Action").center(50, " "), end="|")
         print()
-        print("-"*152)
+        print("-" * 152)
         self.__parse(self.tid_s, data)
 
     def get_result(self):
